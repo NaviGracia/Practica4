36c925735a6f46759df2cedcb49a70afa009c5dd
NaviGracia <119039408+NaviGracia@users.noreply.github.com>
2022-12-09 09:46:35 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+function uno(){
+    var nom = document.getElementById("nombre").value;
+    var cognom = document.getElementById("apellido1").value;
+    alert ("Bienvenido " + nom + " " + cognom)
+}
